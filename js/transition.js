538d81e4e53ff2255dcc6df725463f8f34bac30f
joe <18810544291@163.com>
2015-10-14 22:59:12 +0800

    增加transition.js的注解

--- js/transition.js
+++ js/transition.js
@@ -22,7 +22,7 @@
       OTransition      : 'oTransitionEnd otransitionend',
       transition       : 'transitionend'
     }
-
+    // 通过遍历样式是否存在来判断当前浏览器支持哪种过渡结束事件
     for (var name in transEndEventNames) {
       if (el.style[name] !== undefined) {
         return { end: transEndEventNames[name] }
@@ -31,13 +31,16 @@
 
     return false // explicit for ie8 (  ._.)
   }
-
+  // 用于保证transitionEnd事件一定在过渡完成后被触发
   // http://blog.alexmaccaw.com/css-transitions
   $.fn.emulateTransitionEnd = function (duration) {
+    // transitionEnd是否已触发的标识
     var called = false
     var $el = this
     $(this).one('bsTransitionEnd', function () { called = true })
+    // 如果未触发，则强制触发
     var callback = function () { if (!called) $($el).trigger($.support.transition.end) }
+    // 在过渡结束的时间后检查是否触发
     setTimeout(callback, duration)
     return this
   }
@@ -46,10 +49,11 @@
     $.support.transition = transitionEnd()
 
     if (!$.support.transition) return
-
+    // 自定义事件，把过渡的结束事件，自定义为'bsTransitionEnd'
     $.event.special.bsTransitionEnd = {
       bindType: $.support.transition.end,
       delegateType: $.support.transition.end,
+      // 定义事件处理函数
       handle: function (e) {
         if ($(e.target).is(this)) return e.handleObj.handler.apply(this, arguments)
       }
