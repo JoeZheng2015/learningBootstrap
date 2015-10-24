6dcb92fe03d2d88e5baadbd1420b17b6e55190ad
joe <18810544291@163.com>
2015-10-24 23:43:41 +0800

    完成轮播的分析

--- js/carousel.js
+++ js/carousel.js
@@ -51,7 +51,7 @@
 
     e.preventDefault()
   }
-
+  // 定时的调用next方法，完成自动轮播
   Carousel.prototype.cycle = function (e) {
     e || (this.paused = false)
 
@@ -63,14 +63,20 @@
 
     return this
   }
-
+  /** 通过找到当前item父类下的所有item，然后用$els.index(el)来定位当前item的index */
   Carousel.prototype.getItemIndex = function (item) {
     this.$items = item.parent().children('.item')
     return this.$items.index(item || this.$active)
   }
-
+  /**
+   * 根据方向和当前激活的item，返回下一个item
+   * @param  {string} direction 轮播的方向
+   * @param  {object} active    当前激活的item对象
+   * @return {object}           下一个item
+   */
   Carousel.prototype.getItemForDirection = function (direction, active) {
     var activeIndex = this.getItemIndex(active)
+    // 判断是否wrap，即第一个往前翻或最后一个往后翻
     var willWrap = (direction == 'prev' && activeIndex === 0)
                 || (direction == 'next' && activeIndex == (this.$items.length - 1))
     if (willWrap && !this.options.wrap) return active
@@ -78,8 +84,9 @@
     var itemIndex = (activeIndex + delta) % this.$items.length
     return this.$items.eq(itemIndex)
   }
-
+  // 最终调用slide方法
   Carousel.prototype.to = function (pos) {
+    debugger;
     var that        = this
     var activeIndex = this.getItemIndex(this.$active = this.$element.find('.item.active'))
 
@@ -90,7 +97,7 @@
 
     return this.slide(pos > activeIndex ? 'next' : 'prev', this.$items.eq(pos))
   }
-
+  // 暂停轮播
   Carousel.prototype.pause = function (e) {
     e || (this.paused = true)
 
@@ -103,7 +110,7 @@
 
     return this
   }
-
+  // 用对应参数调用slide方法
   Carousel.prototype.next = function () {
     if (this.sliding) return
     return this.slide('next')
@@ -113,9 +120,14 @@
     if (this.sliding) return
     return this.slide('prev')
   }
-
+  /**
+   * 判断方向，把next移入可视区内
+   * @param  {string}   type 移动的方向
+   * @param  {object} next 将要被激活的item
+   * @return {object}        Carousel实例
+   */
   Carousel.prototype.slide = function (type, next) {
-    var $active   = this.$element.find('.item.active')
+    var $active   = this.$element.find('.item.active') // 当前激活的item
     var $next     = next || this.getItemForDirection(type, $active)
     var isCycling = this.interval
     var direction = type == 'next' ? 'left' : 'right'
@@ -134,7 +146,7 @@
     this.sliding = true
 
     isCycling && this.pause()
-
+    // 如果有bullet，则完成bullet的切换
     if (this.$indicators.length) {
       this.$indicators.find('.active').removeClass('active')
       var $nextIndicator = $(this.$indicators.children()[this.getItemIndex($next)])
@@ -143,9 +155,12 @@
 
     var slidEvent = $.Event('slid.bs.carousel', { relatedTarget: relatedTarget, direction: direction }) // yes, "slid"
     if ($.support.transition && this.$element.hasClass('slide')) {
-      $next.addClass(type)
+      // 下个item，增加类如"next"，使下个item定位在右边
+      $next.addClass(type) 
       $next[0].offsetWidth // force reflow
+      // 当前item，增加类如"left"，使他左移
       $active.addClass(direction)
+      // 下个item再增加类如"left"，使得他左移
       $next.addClass(direction)
       $active
         .one('bsTransitionEnd', function () {
@@ -158,6 +173,7 @@
         })
         .emulateTransitionEnd(Carousel.TRANSITION_DURATION)
     } else {
+      // 若不支持过渡，只是当初的当前item解除激活，激活下个item
       $active.removeClass('active')
       $next.addClass('active')
       this.sliding = false
@@ -208,25 +224,27 @@
   var clickHandler = function (e) {
     var href
     var $this   = $(this)
+    
+    // 找到对应的容器，作为target
     var $target = $($this.attr('data-target') || (href = $this.attr('href')) && href.replace(/.*(?=#[^\s]+$)/, '')) // strip for ie7
     if (!$target.hasClass('carousel')) return
     var options = $.extend({}, $target.data(), $this.data())
     var slideIndex = $this.attr('data-slide-to')
     if (slideIndex) options.interval = false
-
+    // 点击左右箭头时，options.slide为next|prev，这样会在Plugin中调用对应的data[action]（如data['next']），完成相应动作
     Plugin.call($target, options)
-
+    // 若是点击bullet，则直接调用to方法到指定位置
     if (slideIndex) {
       $target.data('bs.carousel').to(slideIndex)
     }
 
     e.preventDefault()
   }
-
+  // 为bullet和箭头绑定同一个点击处理事件
   $(document)
     .on('click.bs.carousel.data-api', '[data-slide]', clickHandler)
     .on('click.bs.carousel.data-api', '[data-slide-to]', clickHandler)
-
+  // 页面加载完后，carousel初始化组件
   $(window).on('load', function () {
     $('[data-ride="carousel"]').each(function () {
       var $carousel = $(this)
